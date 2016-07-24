d36a6b91e7f78e21c17a1eecc40be99952cf5952
nikhilpanchal <nikhilpanchal@gmail.com>
2016-07-24 12:09:40 -0400

    Added some refactoring of the components into container and presentational components

--- React/redux/App.js
+++ React/redux/App.js
@@ -34,6 +34,14 @@ const todos = (state = [], action) => {
 };
 
 const visibility = (state = "SHOW_ALL", action) => {
+	switch(action.type) {
+		case "SET_VISIBILITY_FILTER": {
+			return action.filter;
+		}
+		default: {
+			return state;
+		}
+	}
 	return state;
 }
 
@@ -44,51 +52,163 @@ const reducers = combineReducers({
 
 const store = createStore(reducers);
 
+
+/** Presentational Component **/
+/**
+ * Todo Component. Purely presentational. Contains no business logic.
+ * All business logic passed from the outside.
+ */
+ // The functions are splitting out the incoming prop object keys using
+ // object destructuring.
+ // What is being passed in is the props object, but in the function, 
+ // via destruring, we are only picking up the prop fields that we are
+ // interested in.
+const TodoComp = ({onClick, id, text, done}) => {
+	return (
+		<li key={id} onClick={onClick} style={{
+			textDecoration: 
+				done ? "line-through" : "none"
+		}}>
+			{text}
+		</li>
+	)
+};
+
+const TodoListComp = ({todos, onTodoClick}) => {
+	return (
+	<ul>
+		{todos.map((todo) => 
+			<TodoComp key={todo.id} {...todo} onClick ={ () => onTodoClick(todo.id) }/>
+		)}
+	</ul>
+)}
+
+const AddTodoComp = ({
+	onAddTodo
+}) => {
+	let input;
+	return (
+		<div>
+			<input ref={node => {
+				input = node;
+			}} />
+			<button onClick={() => {
+				onAddTodo(input.value);
+				input.value = '';
+			}}>
+				Add Todo
+			</button>
+		</div>
+	)
+};
+
+
+const FilterLink = ({
+	filter,
+	currentFilter,
+	children,
+	onClick
+}) => {
+	if(currentFilter === filter) {
+		return <span>{children}</span>
+	}
+
+	return (
+		<a href="#" onClick={e => {
+			e.preventDefault();
+			onClick(filter);
+		}}
+		>
+			{children}
+		</a>
+	)
+}
+
+const FooterComp = ({
+	visibility,
+	onFilterClick
+}) => {
+	return (
+		<p>
+			Show: 
+			<FilterLink 
+				filter="SHOW_ALL"
+				currentFilter={visibility}
+				onClick={onFilterClick}>All</FilterLink>
+			{' '}
+			<FilterLink 
+				filter="SHOW_ACTIVE"
+				currentFilter={visibility}
+				onClick={onFilterClick}
+				>Active</FilterLink>
+			{' '}
+			<FilterLink 
+				filter="SHOW_COMPLETED"
+				currentFilter={visibility}
+				onClick={onFilterClick}>Completed</FilterLink>
+		</p>
+	)
+}
+
+const getVisibleTodos = (todos, filter) => {
+	return todos.filter((todo) => {
+		switch(filter) {
+			case "SHOW_ALL": {
+				return true;
+			}
+			case "SHOW_ACTIVE": {
+				return !todo.done;
+			}
+			case "SHOW_COMPLETED": {
+				return todo.done;
+			}
+			default: {
+				return true;
+			}
+		}
+	});
+}
+
+
 let idCounter = 0;
 
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<input ref={node => {
-					this.input = node;
-				}} />
-				<button onClick={() => {
-					store.dispatch({
-						type: 'ADD_TODO',
-						text: this.input.value,
-						id: idCounter++
-					});
-					this.input.value = "";
-				}}>
-					Add Todo
-				</button>
-				<ul>
-					{this.props.todos.map(todo => 
-						<li key={todo.id} onClick={() => {
-							store.dispatch({
-								type: 'TOGGLE_TODO',
-								id: todo.id
-							});
-						}} style={{
-							textDecoration: 
-								todo.done ? "line-through" : "none"
-						}}>
-							{todo.text}
-						</li>
-					)}
-				</ul>
-			</div>
-		)
-	}
-};
+const App = ({todos, visibility}) => (
+	<div>
+		<AddTodoComp onAddTodo={
+			(text) => {
+				store.dispatch({
+					type: 'ADD_TODO',
+					text,
+					id: idCounter++ 
+				})
+			}
+		} />
+		<TodoListComp todos={getVisibleTodos(todos, visibility)} onTodoClick={(id) => {
+			store.dispatch({
+				type: 'TOGGLE_TODO',
+				id
+			})
+		}} />
+		<FooterComp 
+			visibility={visibility}
+			onFilterClick={(filter) => {
+				console.log("Dispatching for filter " + filter);
+				store.dispatch({
+					type: 'SET_VISIBILITY_FILTER',
+					filter
+				})
+			}
+		} />
+	</div>
+);
+
 
 /**
  * Runner that will be invoked from the outside to render this component
  */
 class RunApp {
 	render() {
-		ReactDOM.render(<App todos={store.getState().todos}/>, document.getElementById('app'));
+		ReactDOM.render(<App {...store.getState()}/>, document.getElementById('app'));
 	}
 };
 var runner = new RunApp();
--- React/redux/main.js
+++ React/redux/main.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import { render } from 'react-dom';
+// import { provider } from 'react-redux';
+// import { createStore } from 'redux';
+import todoApp from './reducers';
+import App from './components/App';
 import RunApp from './App';
 
 RunApp.render();
+
+// let store = createStore(todoApp);
+
+// render(
+// 	<Provider store={store}>
+// 		<App />
+// 	</Provider>,
+// 	document.getElementById('app');
+// )
+
+
